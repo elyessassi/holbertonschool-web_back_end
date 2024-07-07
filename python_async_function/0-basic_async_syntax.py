f58fdabc7d156e573696d754fee1442e1946322c
elyessassi <mjoxen@gmail.com>
2024-07-07 12:05:04 +0100

    an asynchronous coroutine that waits for a random tile

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -11,7 +11,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """asynchronous coroutine """
     x = uniform(0, max_delay)
     await asyncio.sleep(x)

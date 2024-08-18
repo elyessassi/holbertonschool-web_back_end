541f265921329ee6af36889b23d1e65a041f6447
elyessassi <mjoxen@gmail.com>
2024-08-18 17:49:11 +0100

    using ES6's for loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
-    let array;
-    const value = array[idx];
-    array[idx] = appendString + value;
+    const newarray = [...array];
+    const value = newarray[idx];
+    newarray[idx] = appendString + value;
   }
 
   return array;

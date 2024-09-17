a17968d07b937601538d2c8e1273c0ff3edbcd21
elyessassi <mjoxen@gmail.com>
2024-09-17 11:35:20 +0100

    basic caching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
+""" Basic Caching """
+
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
+
+    """
+    put: adding a key value pair in the dictionnary
+    get: getting the value of a key specified in the dictionnary
+    """
+
     def put(self, key, item):
-        if (key != None and item != None):
+        if (key is not None and item is not None):
             self.cache_data[key] = item
+
     def get(self, key):
-        if (key != None):
-            return (self.cache_data[key])
-        else:
+        if (key is not None or key not in self.cache_data):
             return (None)
+        else:
+            return (self.cache_data[key])

b26e088fc5cc0843095838f2edc6a58fba74c681
elyessassi <mjoxen@gmail.com>
2024-09-17 18:54:15 +0100

    LRU Caching Algorithm

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -15,13 +15,17 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """ adding an item """
         if (key is not None and item is not None):
-            if (len(self.cache_data) < BaseCaching.MAX_ITEMS):
+            if (key in self.cache_data):
+                del self.cache_data[key]
                 self.cache_data[key] = item
             else:
-                mylist = list(self.cache_data)
-                print(f"DISCARD: {mylist[0]}")
-                del self.cache_data[mylist[0]]
-                self.cache_data[key] = item
+                if (len(self.cache_data) < BaseCaching.MAX_ITEMS):
+                    self.cache_data[key] = item
+                else:
+                    mylist = list(self.cache_data)
+                    print(f"DISCARD: {mylist[0]}")
+                    del self.cache_data[mylist[0]]
+                    self.cache_data[key] = item
 
     def get(self, key):
         """ displays an item of a certain key"""

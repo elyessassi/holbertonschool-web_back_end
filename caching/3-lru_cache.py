55b9d9c94e2ac51d0cc9a73c02740a3de9162ecc
elyessassi <mjoxen@gmail.com>
2024-09-17 18:56:31 +0100

    LRU Caching Algorithm

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -18,10 +18,10 @@ class LRUCache(BaseCaching):
             if (key in self.cache_data):
                 del self.cache_data[key]
                 self.cache_data[key] = item
-            else:
-                if (len(self.cache_data) < BaseCaching.MAX_ITEMS):
+        else:
+            if (len(self.cache_data) < BaseCaching.MAX_ITEMS):
                     self.cache_data[key] = item
-                else:
+            else:
                     mylist = list(self.cache_data)
                     print(f"DISCARD: {mylist[0]}")
                     del self.cache_data[mylist[0]]

aa181b12f101226e499be88b473aab7a561bd0cb
elyessassi <mjoxen@gmail.com>
2024-10-03 21:09:11 +0100

    function used to obstruct the log message and return it

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+
 """ file that containes function used
 to obstruct the log message and return it """
+
+
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
     """ function used to obstruct the log message and return it """
     for i in fields:
-        regexString = f"(?<={i}=)[^{separator}]+"
-        message = re.sub(regexString, redaction, message)
+        message = re.sub(f"(?<={i}=)[^{separator}]+", redaction, message)
     return message

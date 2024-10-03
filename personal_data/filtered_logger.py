2b79933b5bd489edfc35e38bbe8054b9696b32ad
elyessassi <mjoxen@gmail.com>
2024-10-03 21:15:12 +0100

    adding annotation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ to obstruct the log message and return it """
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str):
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     """ function used to obstruct the log message and return it """
     for i in fields:
         message = re.sub(f"(?<={i}=)[^{separator}]+", redaction, message)

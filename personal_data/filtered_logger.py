df65023e2f61fb4dfa8b032133be816500c2122f
elyessassi <mjoxen@gmail.com>
2024-10-05 10:11:27 +0100

    fixing annotation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -12,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str,
     for i in fields:
         message = re.sub(f"(?<={i}=)[^{separator}]+", redaction, message)
     return message
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        logging.basicConfig(format=self.FORMAT, level=logging.INFO)
+        message = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        message = logging.info(message)
+        print("****** ",message,"*****")
+        return message

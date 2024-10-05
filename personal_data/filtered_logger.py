6772cd438cdb7e862d4598a0576c581e1c84126b
elyessassi <mjoxen@gmail.com>
2024-10-05 09:28:11 +0100

    fixing annotation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,12 +2,14 @@
 """ file that containes function used
 to obstruct the log message and return it """
 import re
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ function used to obstruct the log message and return it """
 
     for i in fields:
         message = re.sub(f"(?<={i}=)[^{separator}]+", redaction, message)
     return message
+

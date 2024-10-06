e6d6404a760cacf01ce8eaaef260e6f182fc4342
elyessassi <mjoxen@gmail.com>
2024-10-06 18:33:10 +0100

    adding is_valid function

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     byteString = password.encode()
     hashedPWD = bcrypt.hashpw(byteString, bcrypt.gensalt())
     return hashedPWD
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ function to validate if the hashed password
+     matches the real password """
+    byteString = password.encode()
+    return bcrypt.checkpw(byteString, hashed_password)

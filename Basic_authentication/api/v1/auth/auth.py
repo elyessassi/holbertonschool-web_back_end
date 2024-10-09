2014e31dd61cba63481e8d6ee5f7304c7f309cdf
elyessassi <mjoxen@gmail.com>
2024-10-09 20:22:53 +0100

    adding authentication class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" authentication file """
+from flask import request
+from typing import List
+from typing import TypeVar
+
+
+class Auth():
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to check if accessing a path
+        requires authentication """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to check the authorization header """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to check the current user """
+        return None

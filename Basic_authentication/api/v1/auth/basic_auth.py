ca1bb83488853b0fb90f98f7f05f178dd14e7d5f
elyessassi <mjoxen@gmail.com>
2024-10-10 13:14:31 +0100

    adding a method to extract base64 authorization header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ basic method class"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        length = len(authorization_header)
+        return authorization_header[6:]
+        
+
+        

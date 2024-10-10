89526352af8af5f87586cc4a977b9989410c9278
elyessassi <mjoxen@gmail.com>
2024-10-10 12:35:09 +0100

    adding before_request decorator and using AUTH_TYPE environment variable

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,9 +9,15 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth = getenv("AUTH_TYPE")
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +39,20 @@ def forbid(error) -> str:
     """ Forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
+
+# Before the request is sent
+@app.before_request
+def beforeRequestf():
+    """ methods to run before the request is sent """
+    if auth is not None:
+        if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+
+
+
 
 
 if __name__ == "__main__":

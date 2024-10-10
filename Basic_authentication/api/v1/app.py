fdc252252e0fe5e5d129ef68e8c5926f9d637ea9
elyessassi <mjoxen@gmail.com>
2024-10-10 12:46:33 +0100

    adding a condition to check if AUTH_TYPE has a value

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,8 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = getenv("AUTH_TYPE")
-if auth:
+if getenv("AUTH_TYPE"):
     from api.v1.auth.auth import Auth
     auth = Auth()
 

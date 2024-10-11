0384778580f444d62666995c4e26ebc376d93309
elyessassi <mjoxen@gmail.com>
2024-10-11 17:47:48 +0100

    adding a method to decode the base64 header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,13 +2,14 @@
 """ Basic authentication module """
 from api.v1.auth.auth import Auth
 import base64
+import binascii
 
 
 class BasicAuth(Auth):
     """ basic method class"""
     def extract_base64_authorization_header(self,
-                                            authorization_header: str
-                                            ) -> str:
+                                            authorization_header:
+                                            str) -> str:
         """ Method to extract base64 authorization header """
         if authorization_header is None:
             return None
@@ -17,3 +18,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return (authorization_header[6:])
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ method to decode base64 """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        else:
+            string = base64.b64decode(base64_authorization_header)
+            return string.decode("ascii")

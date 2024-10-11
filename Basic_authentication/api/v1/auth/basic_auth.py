732d7e0757734e314d095e9d0f54dfe4af2108e9
elyessassi <mjoxen@gmail.com>
2024-10-11 18:02:58 +0100

    adding a method to extract user credentials form the decoded authorization header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,23 @@ class BasicAuth(Auth):
         else:
             string = base64.b64decode(base64_authorization_header)
             return string.decode("ascii")
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str
+                                 ) -> (str, str):
+        """ method to extract user credentials
+         form the decoded authorization header
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        index = decoded_base64_authorization_header.find(":")
+        # index is the index of ':' in the string
+        if index == -1:
+            return (None, None)
+        else:
+            # will return the part before ':'
+            # and after ':'
+            return (decoded_base64_authorization_header[:index],
+                    decoded_base64_authorization_header[index + 1:])

a5167a6f3b6ed56874218dc644aebdbe335ab83f
elyessassi <mjoxen@gmail.com>
2024-10-13 20:30:43 +0100

    handling case where auth.authorization_header(request) and auth.session_cookie(request) return None

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -54,11 +54,15 @@ def beforeRequestf():
         print(auth.current_user(request))
         if auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+            if auth.authorization_header(request) is None:
+                if auth.session_cookie(request) is None:
+                    abort(401)
 
 
 if __name__ == "__main__":

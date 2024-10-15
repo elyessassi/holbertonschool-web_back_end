7cc0388040f6c439a7ec24966f0048154d806d8d
elyessassi <mjoxen@gmail.com>
2024-10-15 19:01:43 +0100

    module to create a db and a method to add a user

---
+++ user_authentication_service/db.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import User
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+    users_number = 0
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Method tha creates a user """
+        self.users_number += 1
+        return User(id=self.users_number, email=email, hashed_password=hashed_password)

d24bbdc5d63182b7c243e0ad864152a0a60b208d
elyessassi <mjoxen@gmail.com>
2024-10-23 16:52:17 +0100

    adding a class to test utils.get_json() method

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,11 +2,12 @@
 """Test file for utils.py"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Class to test utils.py"""
+    """Class to test utils.access_nested_map method"""
     @parameterized.expand([
         ({"a": 1}, ("a",), (1)),
         ({"a": {"b": 2}}, ("a",), ({"b": 2})),
@@ -14,7 +15,8 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """  Method that tests the
-          test_access_nested_map method  """
+          test_access_nested_map method
+          if method is correct  """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -26,3 +28,17 @@ class TestAccessNestedMap(unittest.TestCase):
             raises a key error if the index is wrong """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class to test utils.get_json method"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, url, payload, mocked_get_request):
+        mocked_get_request.json.return_value = payload
+        mocked_get_request(url)
+        mocked_get_request.assert_called_once_with(url)
+        self.assertEqual(mocked_get_request.json(), payload)

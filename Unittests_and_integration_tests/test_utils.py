cf9203f3c7e04b8f55695c5ec58c3d89c39473d3
elyessassi <mjoxen@gmail.com>
2024-10-23 15:34:40 +0100

    adding a method that tests if  test_access_nested_map method raises a key error if the index is wrong

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -13,6 +13,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), (2))
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Method that tests the
+        """  Method that tests the
           test_access_nested_map method  """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), (KeyError)),
+        ({"a": 1}, ("a", "b"), (KeyError))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Method that tests if  test_access_nested_map method
+            raises a key error if the index is wrong """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)

f5f8db64372f11d3818a93b7b07ad39f4a931481
elyessassi <mjoxen@gmail.com>
2024-10-24 23:38:34 +0100

    adding a method that tests the_public_repo_url method

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 from client import GithubOrgClient
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -16,9 +16,21 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_org(self, org_name, expected, mocked_get_json):
-        """ Module that checks if the org method works properly """
+        """ Method that checks if the org method works properly """
         mocked_get_json.return_value = expected
         x = GithubOrgClient(org_name)
         x.org
         self.assertEqual(x.org, expected)
         mocked_get_json.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ Method that tests the
+            _public_repo_url method
+        """
+        with patch("client.GithubOrgClient.org", new_callable=PropertyMock,
+                   return_value={'repos_url':
+                                 'https://api.github.com/orgs/myorg/repos',
+                                 'id': 1234}):
+            x = GithubOrgClient("hello")
+            self.assertEqual("https://api.github.com/orgs/myorg/repos",
+                             x._public_repos_url)

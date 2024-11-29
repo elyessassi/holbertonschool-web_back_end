ef2f7df79bdb784e9acadb48c1abb47e50bfe5b6
elyessassi <mjoxen@gmail.com>
2024-11-29 18:43:44 +0100

    adding the handler finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,15 +1,12 @@
 function handleResponseFromAPI(promise) {
   promise.then(() => {
     const obj = { status: 200, body: 'success' };
-    console.log('Got a response from the API');
     return (obj);
-  },
-).catch(
-  () => {
+  }).catch(
+    () => new Error()
+  ).finally(() => {
     console.log('Got a response from the API');
-    return new Error;
-  }
-);
+  });
 }
 
 export default handleResponseFromAPI;

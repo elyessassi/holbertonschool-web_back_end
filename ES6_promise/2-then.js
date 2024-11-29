49522789324b93f62fbd9eda63a80e0e9d4f10f7
elyessassi <mjoxen@gmail.com>
2024-11-29 18:56:43 +0100

    using then with 2 arguments and finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,11 +2,10 @@ function handleResponseFromAPI(promise) {
   promise.then(() => {
     const obj = { status: 200, body: 'success' };
     return (obj);
-  }).catch(
-    () => new Error()
-  ).finally(() => {
-    console.log('Got a response from the API');
-  });
+  }, () => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
 
 export default handleResponseFromAPI;

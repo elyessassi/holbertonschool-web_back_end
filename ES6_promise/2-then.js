ea0f22864428cf9b54fb8739b8771d39daa2b0c2
elyessassi <mjoxen@gmail.com>
2024-11-29 18:24:07 +0100

    creating a promise that returns an obj when resolved and returns an error when rejected and logs a message

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    const obj = { status: 200, body: 'success' };
+    console.log('Got a response from the API');
+    return (obj);
+  },
+  () => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
+}
+
+export default handleResponseFromAPI;

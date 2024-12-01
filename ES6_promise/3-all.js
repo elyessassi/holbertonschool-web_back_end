815997a6aab91d8a2cba732ed17a8071230d348a
elyessassi <mjoxen@gmail.com>
2024-12-01 06:01:09 +0100

    handling multiple promises at the same time

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
   const arr = [uploadPhoto(), createUser()];
-  Promise.all(arr).then((data) => { console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
+  return Promise.all(arr).then((data) => { console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
 }
 
 export default handleProfileSignup;

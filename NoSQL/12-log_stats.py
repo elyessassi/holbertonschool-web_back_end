5cdb6ca28e4ebd86edc75b80c4904c8be7e8e7c0
elyessassi <mjoxen@gmail.com>
2025-08-11 18:53:43 +0100

    show stats about Nginx logs stored in MongoDB

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+""" show stats about Nginx logs """
 
 from pymongo import MongoClient
+
+""" show stats about Nginx logs """
 
 client = MongoClient('mongodb://127.0.0.1:27017')
 
